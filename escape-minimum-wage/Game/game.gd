aa3365f371fa86c885dac750e5496add774e620a
Chase Odom <chasemichaelodom@gmail.com>
2025-07-11 21:38:46 -0600

    Adding a UI and pause button

--- escape-minimum-wage/Game/game.gd
+++ escape-minimum-wage/Game/game.gd
@@ -3,6 +3,10 @@ class_name Game extends Control
 @export var WorldNode: World
 @export var background_tiles: ShaderMaterial
 
+@export var Pause: Button
+@export var Settings: Button
+@export var PauseScreen: ColorRect
+
 const LevelDataTypes := {
 		LEVEL = "level",
 		WAGE = "wage",
@@ -23,6 +27,20 @@ func _ready() -> void:
 	playerdata_updated.connect(WorldNode.PlayerNode.on_playerdata_updated)
 	playerdata_updated.connect(WorldNode.EnemySpawnerNode.on_playerdata_updated)
 	update_playerdata(PlayerResource.new())
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("pause"):
+		toggle_pause()
+
+func toggle_pause() -> void:
+	get_tree().paused = not get_tree().paused
+	PauseScreen.visible = get_tree().paused
+	if get_tree().paused:
+		Pause.icon = preload("uid://dvv3o43uodmbs")
+		Settings.icon = preload("uid://cq8ug5bepd84n")
+	else:
+		Pause.icon = preload("uid://cvy8hb0kkcmuv")
+		Settings.icon = preload("uid://dtvw1irhowm2l")
 
 func _process(_delta: float) -> void:
 	background_tiles.set_shader_parameter("offset", WorldNode.PlayerNode.position)
