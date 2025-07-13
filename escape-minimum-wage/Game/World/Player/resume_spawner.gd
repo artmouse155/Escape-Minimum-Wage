8278cc5388048c9b760c45c63dacd2fc646c71aa
Chase Odom <chasemichaelodom@gmail.com>
2025-07-12 20:25:06 -0600

    You can change the number of resumes thrown!

--- escape-minimum-wage/Game/World/Player/resume_spawner.gd
+++ escape-minimum-wage/Game/World/Player/resume_spawner.gd
@@ -2,10 +2,14 @@ class_name ResumeSpawner extends Node2D
 
 @export var ResumeScene : PackedScene
 
+const SPREAD_RAD := 0.1 # Spread in radians
+
 # We get this from the incoming playerData
 var resume_spawn_rate := 0.0 # Resumes to throw per second
 var resume_speed := 0.0
+
 var resume_damage := 0.0
+var thrown_resumes := 1
 
 var time_counter := 0.0
 
@@ -14,8 +18,13 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func spawn_resumes(num : int):
+	var direction = global_position.direction_to(get_global_mouse_position())
+	var total_gap = (num - 1) * SPREAD_RAD
+	var half_gap = total_gap / 2
+	direction = direction.rotated(-half_gap)
 	for i in range(num):
-		spawn_resume(global_position.direction_to(get_global_mouse_position()))
+		spawn_resume(direction)
+		direction = direction.rotated(SPREAD_RAD)
 
 func spawn_resume(direction: Vector2):
 	assert(ResumeScene, "No ResumeScene")
@@ -27,7 +36,7 @@ func _process(delta: float) -> void:
 	time_counter += delta
 	if time_counter > resume_spawn_rate:
 		time_counter = time_counter - resume_spawn_rate
-		spawn_resumes(1)
+		spawn_resumes(thrown_resumes)
 
 func on_playerdata_updated(playerdata : PlayerResource):
 	resume_spawn_rate = playerdata.resume_spawn_rate
@@ -37,3 +46,9 @@ func on_playerdata_updated(playerdata : PlayerResource):
 		resume_damage = float(resume_tier.values["impact"])
 	else:
 		resume_damage = 0
+	
+	var networking := playerdata.get_upgrade(Shop.UpgradeTypes.NETWORKING)
+	if networking.values.has("thrown_resumes"):
+		thrown_resumes = int(networking.values["thrown_resumes"])
+	else:
+		thrown_resumes = 0
